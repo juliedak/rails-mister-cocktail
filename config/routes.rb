eef44b0d9be5d00e6b27bbdc168016050c6c4f48
juliedakouré <julie.dakoure@outlook.fr>
2020-02-13 14:35:58 +0100

    Routing, Controller, Views for Doses

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-resources :cocktails, only: [:show, :index, :create, :new]
+  resources :cocktails, only: [:show, :index, :create, :new] do |variable|
+    resources :doses, only: [:create, :new]
+  end
+  resources :doses, only: :destroy
 end
